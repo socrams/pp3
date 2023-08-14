bb63e35d29902728b87a00c06c86b78840f23122
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2023-08-14 12:22:27 -0300

    .

--- src/app/response/response.component.ts
+++ src/app/response/response.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { Mensaje } from '../modelo/mensaje';
+import { Component, OnInit } from '@angular/core';
+import { Respuesta } from '../modelo/mensaje';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-response',
   templateUrl: './response.component.html',
   styleUrls: ['./response.component.css']
 })
-export class ResponseComponent {
-respuesta: Mensaje[] = [];
+export class ResponseComponent implements OnInit {
+respuesta: Respuesta[] = [];
 url: string = 'http://localhost:5000/response/'
 
 
@@ -15,12 +15,18 @@ constructor(private http: HttpClient) {
 }
 
 getResponses(){
-  this.http.get<Mensaje>(this.url)
-  .subscribe((data: Mensaje ) => {
-    console.log("data",data);
-    console.log("respuesta:",this.respuesta);
-  });
+  this.http.get<Respuesta>(this.url)
+    .subscribe((data: Respuesta) => {
+      this.respuesta.push(data);
+      //quiero obtener con esto todas las respuestas solamente. y me da undefined.
+      console.log("respuesta:", this.respuesta[0]);
+    });
+    
+}
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  //this.getResponses()
   
 }
-
 }
