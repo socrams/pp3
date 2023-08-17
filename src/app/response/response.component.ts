8384647b6b76d8f25f71192b89c9fee0bef2dff0
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2023-08-17 15:39:39 -0300

    .

--- src/app/response/response.component.ts
+++ src/app/response/response.component.ts
@@ -1,40 +1,63 @@
 import {Component, OnInit} from '@angular/core';
 import { Respuesta } from '../modelo/respuesta';
-
 import {HttpClient} from '@angular/common/http';
+
 @Component({selector: 'app-response', templateUrl: './response.component.html', styleUrls: ['./response.component.css']})
 
 export class ResponseComponent implements OnInit {
     respuestas : Respuesta[] = [];
     respuesta: Respuesta;
     url : string = 'http://localhost:5000/response/';
-    opcionSeleccionadaValue : any = '' ; 
-
+    opcionSeleccionadaValue : number = 0;
+    
     constructor(private http : HttpClient) { 
       this.respuesta = {id: 0, answer: '', response: '', moreOptions: false, moreQuestion: false, options: '' }
     }
-
+mostrarIndex(){
+  //mal los id de ls rstas.
+  console.log('Rsta: ',this.respuestas[this.opcionSeleccionadaValue]);
+  console.log(this.opcionSeleccionadaValue,'opcion seleccionada');
+  console.log(this.respuestas);
+  
+  
+}
     getResponses() {
-        this.http.get<Respuesta>(this.url).subscribe((data : any) => {
+      this.http.get<Respuesta>(this.url).subscribe((data : any) => {
             this.respuestas = data;
         });
     }
-
-    public loadDataById() {
-      this.respuesta = this.getResponseByOrder(0);
-    }
-
-    getResponseByOrder(order: number): Respuesta {
-      return this.respuestas[order];
-    }
-
-    ngOnInit(): void { 
+    ngOnInit() { 
       // Buscamos todas las prespuestas del server.
-        this.getResponses()
+      this.getResponses()
+    }
+    modificar() {
+      if (this.opcionSeleccionadaValue !== undefined && this.opcionSeleccionadaValue >= 0 && this.opcionSeleccionadaValue < this.respuestas.length) {
+        const selectedItem = this.respuestas[this.opcionSeleccionadaValue];
+        this.http.put<Respuesta>(this.url + selectedItem.id, selectedItem).subscribe(
+          data => {
+            console.log('Respuesta modificada con éxito', data);
+          },
+          error => {
+            console.error('Error al modificar la respuesta', error);
+          }
+        );
+      }
+    }
+    // public loadDataById(index: number) {
+    //   console.log(index);
+      
+    //   this.respuesta = this.getResponseByOrder(index);
+    // }
 
-    }
-    opcionSeleccionada(value: any) {
-      this.opcionSeleccionadaValue = value;
-    }
+    // getResponseByOrder(order: number): Respuesta {
+    //   return this.respuestas[order];
+    // }
+
+
+    // opcionSeleccionada(value: any) {
+    //   console.log(value);
+      
+    //   this.opcionSeleccionadaValue = value;
+    // }
     
   }
