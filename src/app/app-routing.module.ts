2fbf5b646910ca17f070599063ae3ccd47f50bfa
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2023-08-24 14:35:30 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-// import { UserComponent } from './user/user.component';
+import { UserComponent } from './user/user.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoutComponent } from './logout/logout.component';
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path:'', component:WelcomeComponent},
   {path:'chat', component:ChatComponent},
   {path:'login', component:LoginComponent},
-  // {path:'user', component:UserComponent},
+  {path:'user', component:UserComponent},
   {path:'userlist', component:RegisterComponent},
   {path:'nav', component:NavbarComponent},
   {path:'logout', component: LogoutComponent},
