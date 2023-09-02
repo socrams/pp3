efd84642c24dcd72ee930942325f4f5069d662be
socrams <82241613+socrams@users.noreply.github.com>
2023-09-02 13:32:39 -0300

    Update config.ts

--- src/app/modelo/config.ts
+++ src/app/modelo/config.ts
@@ -1,2 +1,2 @@
-//export const url = 'https://pp3-python.vercel.app/';
-export const url = 'https://pp3-python-ib6ozjhpx-socrams.vercel.app/';
+export const url = 'https://pp3-python.vercel.app/';
+// export const url = 'https://pp3-python-ib6ozjhpx-socrams.vercel.app/';
