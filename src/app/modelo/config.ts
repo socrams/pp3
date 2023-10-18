e810ac9c1e5112240a62222cfe4873e59ecb1b94
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2023-10-18 19:05:31 -0300

    Update config.ts

--- src/app/modelo/config.ts
+++ src/app/modelo/config.ts
@@ -1,3 +1,3 @@
-export const url = 'http://localhost:5000/';
-//export const url = 'https://pp3-python.vercel.app/';
+//export const url = 'http://localhost:5000/';
+export const url = 'https://pp3-python.vercel.app/';
 // export const url = 'https://pp3-python-ib6ozjhpx-socrams.vercel.app/
